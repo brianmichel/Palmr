5a84acad0ce83aa6cd06b3def06db7e4731c5cdb
krze <krze@tumblr.com>
2016-08-24 18:36:53 -0400

    Test strings in

--- app/PostList.c
+++ app/PostList.c
@@ -7,15 +7,29 @@ void UpdatePostsTable(FormPtr pForm)
 {
   UInt16		obj = FrmGetObjectIndex(pForm, PostListTable);
   TablePtr	table = (TablePtr)FrmGetObjectPtr(pForm, obj);
-  char		*strings[PostListRows] = {"Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Fuck", "Piss"};
+  // char		*strings[PostListRows] = {"Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Fuck", "Piss"};
+  char    *icon[PostListRows] = {"*", "*", "*", "*", "*", "*", "*", "*", "*", "*"};
+  char    *userName[PostListRows] = { "Chungo", "Scrungus", "Meeper", "Sreegs", "Brian", "Ken", "Ello", "I'm", "Oli", "Test"};
+  char    *postPreview[PostListRows] = {  "Cut my life into pieces",
+                                          "This is my last resort",
+                                          "Wake me up inside",
+                                          "I can't wake up",
+                                          "Today I ate a piece of toast",
+                                          "The discourse...",
+                                          "Top ten things I love about squart",
+                                          "This is just sample data",
+                                          "If you're reading this the app is broke",
+                                          "Testing 1 2 3"};
   register	i;
 
   for (i = 0; i < PostListRows; i++)
   {
-    TblSetItemStyle (table, i, 0, numericTableItem);
+    TblSetItemStyle (table, i, 0, labelTableItem);
     TblSetItemStyle (table, i, 1, labelTableItem);
-    TblSetItemInt(table, i, 0, i+1);
-    TblSetItemPtr(table, i, 1, strings[i]);
+    TblSetItemStyle (table, i, 2, labelTableItem);
+    TblSetItemPtr(table, i, 0, icon[i]);
+    TblSetItemPtr(table, i, 1, userName[i]);
+    TblSetItemPtr(table, i, 2, postPreview[i]);
     TblSetRowUsable(table, i, true);
   //OS3.5:		TblSetRowMasked(table, i, ((i%2) == 0) ? true : false);
   //		TblSetRowSelectable(table, i, ((i%2) == 0) ? false : true);
