5ba534edeca966ac512fffbfc503c3890080b39d
Brian Michel <brian.michel@gmail.com>
2016-08-24 16:49:58 -0400

    Adding TextPost struct

---
+++ app/TextPost.h
@@ -0,0 +1,13 @@
+#ifndef _TEXTPOST_H_
+#define _TEXTPOST_H_
+
+#include "PostTypes.h"
+
+static const int TextPostAuthorMax = 80;
+static const int TextPostBodyMax = 100;
+
+typedef struct TextPost {
+
+} TextPost;
+
+#endif
